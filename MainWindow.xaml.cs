25f65fe5fe4f3a0e484d5634b44f050761407bc1
KY <you_kissaki@163.com>
2019-08-05 09:25:43 +0800

    yf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Timers;
+
 
 namespace WpfApp3
 {
@@ -18,6 +20,7 @@ namespace WpfApp3
             MainWindowColorChange();
             InitializeComponent();
         }
+
         public string buttonContentLand = "土地";
         public string buttonContentFish = "鱼塘";
         public string buttonContentShop = "商店";
@@ -30,11 +33,11 @@ namespace WpfApp3
 
 
             //MainWindow所有Grid的颜色
-            
-            //cropbutton.Background = ;
+
+            //landbutton.Background = Color.FromRgb(0,0,255);
 
 
-            //crop_fish_shop_Grid.Background = cropbutton.Background;
+            //crop_fish_shop_Grid.Background = landbutton.Background;
             //crop_fish_shop_Grid.Background = fishbutton.Background;
             //crop_fish_shop_Grid.Background = shopbutton.Background;
 
@@ -45,34 +48,162 @@ namespace WpfApp3
         }
 
 
+
         private void Landbutton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                TabHeader();
+                TxtText();
+                Item2Visible();
+                Item3Visible();
 
-            item1Tab.Header = buttonContentLand;
-            //crop_fish_shop_Grid.Visibility = Visibility.Visible;
-            //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
-            //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
-            crop_fish_shop_TXT.Text = buttonContentLand;
+
+
+
+
+                void TabHeader()
+                {
+                    item1Tab.Header = buttonContentLand;
+                }
+
+                //crop_fish_shop_Grid.Visibility = Visibility.Visible;
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+
+
+
+                void TxtText()
+                {
+                    crop_fish_shop_TXT.Text = buttonContentLand;
+                }
+
+                void Item2Visible()
+                {
+                    item2Tab.Visibility = Visibility.Visible;
+                }
+
+                void Item3Visible()
+                {
+                    item3Tab.Visibility = Visibility.Visible;
+                }
+            }
+            catch (System.Exception)
+            {
+
+                
+            }
+            
+
         }
 
         private void Fishbutton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                TabHeader();
+                TxtText();
+                Item2Visible();
+                Item3Visible();
 
-            item1Tab.Header = buttonContentFish;
-            //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
-            //crop_fish_shop_Grid.Visibility = Visibility.Visible;
-            //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
-            crop_fish_shop_TXT.Text = buttonContentFish;
+
+
+
+
+                void TabHeader()
+                {
+                    item1Tab.Header = buttonContentFish;
+                }
+
+                //crop_fish_shop_Grid.Visibility = Visibility.Visible;
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+
+
+
+                void TxtText()
+                {
+                    crop_fish_shop_TXT.Text = buttonContentFish;
+                }
+
+
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+                //crop_fish_shop_Grid.Visibility = Visibility.Visible;
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+
+                void Item2Visible()
+                {
+                    item2Tab.Visibility = Visibility.Hidden;
+                }
+
+                void Item3Visible()
+                {
+                    item3Tab.Visibility = Visibility.Hidden;
+                }
+            }
+            catch (System.Exception)
+            {
+
+                
+            }
+
+            
+            
         }
 
         private void Shopbutton_Click(object sender, RoutedEventArgs e)
         {
 
-            item1Tab.Header = buttonContentShop;
-            //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
-            //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
-            //crop_fish_shop_Grid.Visibility = Visibility.Visible;
-            crop_fish_shop_TXT.Text = buttonContentShop;
+            try
+            {
+                TabHeader();
+                TxtText();
+                Item2Visible();
+                Item3Visible();
+
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+                //crop_fish_shop_Grid.Visibility = Visibility.Collapsed;
+                //crop_fish_shop_Grid.Visibility = Visibility.Visible;
+
+
+                // Tab的header
+                void TabHeader()
+                {
+                    item1Tab.Header = buttonContentShop;
+                }
+
+                // ======end
+
+                // 文本内容
+                void TxtText()
+                {
+                    crop_fish_shop_TXT.Text = buttonContentShop;
+                }
+
+                // ======end
+
+                // 设置itemTab的可见性
+                void Item2Visible()
+                {
+                    item2Tab.Visibility = Visibility.Visible;
+                }
+
+                void Item3Visible()
+                {
+                    item3Tab.Visibility = Visibility.Visible;
+                }
+
+                // itemtab======end
+
+
+            }
+            catch (System.Exception)
+            {
+
+                
+            }
+            
+            
         }
     }
 }
